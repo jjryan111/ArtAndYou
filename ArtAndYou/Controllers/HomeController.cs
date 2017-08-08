973fe72b26e51b0e416426b3053ad37778916870
JJ Ryan <jryan@motor.com>
2017-08-08 10:39:53 -0400

    Added Choices

--- ArtAndYou/Controllers/HomeController.cs
+++ ArtAndYou/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ArtAndYou.Models;
 
 namespace ArtAndYou.Controllers
 {
@@ -29,6 +30,16 @@ namespace ArtAndYou.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Medium()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Choice(Medium r)
+        {
+            return View(r.medium);
         }
     }
 }
---
+++ ArtAndYou/Models/Medium.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+namespace ArtAndYou.Models
+{
+    public class Medium
+    {
+        [Required]
+        [Key]
+        public string medium { get; set; }
+        [Key]
+        public string firstName { get; set; }
+        public Medium()
+        {
+            this.medium = medium;
+            this.firstName = firstName;
+        }
+    }
+}
