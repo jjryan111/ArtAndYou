be689ff9c962563147b7663b2f34f8651c574dbd
Christopher Helgeson <ChristopherHelgeson@hotmail.com>
2017-08-08 11:17:50 -0400

    add Christopher's work

--- ArtAndYou/Controllers/HomeController.cs
+++ ArtAndYou/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using ArtAndYou.Models;
 
 namespace ArtAndYou.Controllers
 {
@@ -10,7 +16,68 @@ namespace ArtAndYou.Controllers
     {
         int christopher = 0;
         int thing = 0;
-        
+
+
+        string size = "?height=300&width=300";
+        string urlHeader = "http://api.harvardartmuseums.org";
+
+        string APIkey = "&apikey=db4038a0-79da-11e7-aa25-e32c9c02c857";
+
+        public ActionResult Portfolio()
+        {
+            //string param = "/object?person=33430";
+            string param = "/object?classification=Photographs&hasimage=1";
+
+            HttpWebRequest request = WebRequest.CreateHttp("http://api.harvardartmuseums.org" + param + APIkey);
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string ApiText = rd.ReadToEnd();
+            JObject o = JObject.Parse(ApiText);
+            string portfolio = "";
+            for (int i = 0; i < 10; i++)
+            {
+                portfolio += o["records"][i]["objectid"] + " ";
+            }
+            ViewBag.ObjectID = portfolio;
+            return View();
+        }
+
+        public ActionResult Image()
+        {
+            string param = "/object?person=33430";
+
+            HttpWebRequest request = WebRequest.CreateHttp("http://api.harvardartmuseums.org" + param + APIkey);
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string ApiText = rd.ReadToEnd();
+            JObject o = JObject.Parse(ApiText);
+            //ViewBag.ApiText = o["url"];
+
+            //for use with one image:
+            ViewBag.ApiText = o["records"][0]["images"][0]["baseimageurl"] + size;
+            return View();
+        }
+
+        public ActionResult Data(Queries q)
+        {
+            //string call = q.SearchURL;
+            //HttpWebRequest request = WebRequest.CreateHttp("http://api.harvardartmuseums.org" + param + APIkey);
+            //HttpWebRequest request = WebRequest.CreateHttp("http://api.harvardartmuseums.org/object?aggregation={records:{people:{culture:American}}}" + APIkey);
+
+            //HttpWebRequest request = WebRequest.CreateHttp("\"" + call + "\"");
+            //request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //StreamReader rd = new StreamReader(response.GetResponseStream());
+            //string ApiText = rd.ReadToEnd();
+            //JObject o = JObject.Parse(ApiText);
+            //ViewBag.ApiText = o["url"];
+            //ViewBag.search = call;
+            //for use with one image:
+            //ViewBag.ApiText = o["images"][0]["baseimageurl"] + size;
+            return View();
+        }
 
         public ActionResult Index()
         {
