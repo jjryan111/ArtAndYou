40d2476c8dcd35ac1d478d5b7cfe3dcea8a2c416
JJ Ryan <jryan@motor.com>
2017-08-10 15:01:21 -0400

    Survey Question Pages

--- ArtAndYou/Controllers/SurveyController.cs
+++ ArtAndYou/Controllers/SurveyController.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ArtAndYou.Controllers;
+using ArtAndYou.Models;
+using System.Net;
 
 namespace ArtAndYou.Controllers
 {
     public class SurveyController : Controller
     {
+        public string name;
+        public string cent;
+        public string cult;
+        public string classification;
+
+        string thing = "";
+        private ArtInfoEntities1 db = new ArtInfoEntities1();
+
+     
+        // GET: UserInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+        public  ActionResult Choice1(Survey M)
+        {
+            string concat = M.ConCatChoice(M.question, M.classification, M.cent);
+            return View(concat);
+        }
+
         // GET: Survey
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SurveyQ3PaintCult()
+        {
+            return View();
+        }
+        public ActionResult SurveyQ2PaintCent()
+        {
+            return View();
+        }
+
+        public ActionResult SurveyQ1Classification()
         {
             return View();
         }
